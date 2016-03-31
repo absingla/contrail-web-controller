d61414a77f9fba58c32b02e5360af444f4b399b4
Sarin Kizhakkepurayil <sarink@juniper.net>
2016-03-30 18:47:20 -0700

    Related-Bug: #1553413 Fix random timeout issue with UT
    - instanceView renders all tab on pageload. for test page, manually fire
    onAllViewsRenderComplete since testInitFn is implemented.
    - fixed leading '/' in resource url path for css
    - added css paths to be pulled when karma starts
    
    Change-Id: Iaad7c69f0f5b4d6771b3902b4a7915ec6cce6427

--- webroot/test/ui/Gruntfile.js
+++ webroot/test/ui/Gruntfile.js
@@ -37,11 +37,13 @@ module.exports = function (grunt) {
 
         {pattern: 'contrail-web-controller/webroot/test/ui/ct.test.app.js'},
         {pattern: 'contrail-web-controller/webroot/test/ui/*.js', included: false},
+        {pattern: 'contrail-web-controller/webroot/monitor/**/*.css', included: false},
         {pattern: 'contrail-web-controller/webroot/monitor/**/*.tmpl', included: false},
         {pattern: 'contrail-web-controller/webroot/common/ui/templates/*.tmpl', included: false},
         {pattern: 'contrail-web-controller/webroot/common/**/{!(*.test.js), !(*.unit.test.js)}', included: false},
 
         //For built dir
+        {pattern: 'contrail-web-controller/webroot/built/monitor/**/*.css', included: false},
         {pattern: 'contrail-web-controller/webroot/built/monitor/**/*.tmpl', included: false},
         {pattern: 'contrail-web-controller/webroot/built/common/ui/templates/*.tmpl', included: false},
         {pattern: 'contrail-web-controller/webroot/built/common/**/{!(*.test.js), !(*.unit.test.js)}', included: false},
