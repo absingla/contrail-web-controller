e8d6b8e5942c1991f52922398c25ec12806bd71f
sadhana124 <sadhana.412@gmail.com>
2016-07-25 13:24:44 -0700

    Bootstrap3 style fixes for mini-menu, NM graph, advanced query form elements

--- webroot/reports/qe/ui/js/views/FlowSeriesFormView.js
+++ webroot/reports/qe/ui/js/views/FlowSeriesFormView.js
@@ -182,7 +182,7 @@ define([
                                                     label: false,
                                                     path: 'time_granularity',
                                                     dataBindValue: 'time_granularity',
-                                                    class: "col-xs-4",
+                                                    class: "col-xs-5",
                                                     elementConfig: {min: 1}
                                                 }
                                             },
